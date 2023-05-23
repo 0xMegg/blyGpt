84f46cdd524f744fea43968294dec328bde8982b
김준엽 <finero91@gmail.com>
2023-05-23 18:07:07 +0900

    in a progress to add a filter about farm function

--- src/functions/farmfunction.js
+++ src/functions/farmfunction.js
@@ -10,12 +10,40 @@ const { Pagination, ExtraRowPosition } = require("pagination.djs");
 const Canvas = require("@napi-rs/canvas");
 
 async function farmfunction(interaction) {
+  const cropRef = db
+    .collection("users")
+    .doc(interaction.user.id)
+    .collection("myFarm")
+    .doc("crop1");
+  const cropDoc = await cropRef.get();
+  const cropData = cropDoc.data();
+  const type = cropData.type;
+  const time = cropData.createAt._seconds;
+
+  switch (type) {
+    case 1:
+      const now = new Date().getTime() / 1000;
+      const gap = now - time;
+      console.log(gap);
+      break;
+    case 2:
+      console.log("case2");
+      break;
+    default:
+      console.log("에러에러");
+      break;
+  }
+
   const base =
     "https://cdn.discordapp.com/attachments/1110128243220172833/1110398686569177208/Sprite-0001-non.png";
   const assets =
     "https://cdn.discordapp.com/attachments/1110128243220172833/1110398742508605440/assets_3232.png";
   const seed =
-    "https://cdn.discordapp.com/attachments/1110128243220172833/1110413346844459039/seed.png";
+    "https://cdn.discordapp.com/attachments/1110128243220172833/1110478589809401886/seed.png";
+  const sprout =
+    "https://cdn.discordapp.com/attachments/1110128243220172833/1110478589255753728/sprout.png";
+  const carrot =
+    "https://cdn.discordapp.com/attachments/1110128243220172833/1110478589490643025/carrot.png";
   const canvas = Canvas.createCanvas(512, 288);
   const context = canvas.getContext("2d");
 
--- src/functions/shopfunction.js
+++ src/functions/shopfunction.js
@@ -22,16 +22,31 @@ async function startfunction(interaction) {
 
   const collector = interaction.channel.createMessageComponentCollector({
     componentType: ComponentType.Button,
-    time: 60000,
+    // time: 60000,
   });
 
-  collector.on("collect", (i) => {
+  collector.on("collect", async (i) => {
     if (i.user.id === interaction.user.id) {
+      const docRef = db.collection("users").doc(i.user.id);
+      const doc = await docRef.get();
+      const docData = doc.data();
+      console.log(doc.data().gold);
+
       i.reply(`${i.user.id} clicked on the ${i.customId} button.`);
       switch (i.customId) {
         case "item1":
-          interaction.channel.send("purchased");
+          docRef.update({ gold: docData.gold - 1 });
+          const newCollectionRef = docRef.collection("myFarm");
+          const newDocRef = newCollectionRef.doc("crop1");
+          docRef.update({ exp: 1 });
+          // Add data to the new document
+          await newDocRef.set({
+            type: 1,
+            createAt: new Date(),
+          });
+          interaction.channel.send("item1 button acted");
           break;
+
         default:
           interaction.channel.send("not item1");
           break;
